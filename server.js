37e39091b2fd9434c3d078010ad06ef45bc5c406
saad-mamun <mamunali88991@gmail.com>
2026-01-11 23:13:39 +0600

    req.params

--- server.js
+++ server.js
@@ -2,27 +2,43 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT;
 
-//setup static folder....
-app.use(express.static(path.join(__dirname, "public")))
+// role-2 setup static folder....
+// app.use(express.static(path.join(__dirname, "public")))
 
-// file routing....
+// role -1 file routing....
 // app.get("/", (req, res) => {
-//     // file director
+//   // file director
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 //   // res.send('<h1>Hello world</h1>')
 //   //   res.send({ message: "Hello, World!" });
 // });
 // app.get("/about", (req, res) => {
-//     // file director 
+//   // file director
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// role 3 - json
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+//role 4 - id
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
