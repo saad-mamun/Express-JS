d811ab0781ea88127a13b2ba121799d7583b5f22
saad-mamun <mamunali88991@gmail.com>
2026-01-11 22:31:29 +0600

    staticFolder

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-    // file director
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-  // res.send('<h1>Hello world</h1>')
-  //   res.send({ message: "Hello, World!" });
-});
+//setup static folder....
+app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/about", (req, res) => {
-    // file director 
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+// file routing....
+// app.get("/", (req, res) => {
+//     // file director
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+//   // res.send('<h1>Hello world</h1>')
+//   //   res.send({ message: "Hello, World!" });
+// });
+// app.get("/about", (req, res) => {
+//     // file director 
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
